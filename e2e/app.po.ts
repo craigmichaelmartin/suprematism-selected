craigmichaelmartin/suprematism-card-navbar

repository generated with base-component

e2e/app.po.ts
@@ -1,0 +1,12 @@
+import { browser, element, by } from 'protractor';
+
+export class SuprematismCardNavbarPage {
+
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('supre-root h1')).getText();
+  }
+}

craigmichaelmartin/suprematism-selected

e2e/app.po.ts
@@ -1,0 +1,12 @@
+import { browser, element, by } from 'protractor';
+
+export class SuprematismSelectedPage {
+
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('supre-root h1')).getText();
+  }
+}